e04cfe8acec2c7ffd837332cf18064ad762c5245
Roger Silva <orogersilva@gmail.com>
2018-05-30 21:07:07 -0300

    First commit.

---
+++ app/src/main/java/com/orogersilva/spotmusicalarm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.orogersilva.spotmusicalarm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
