d1e5d3f465a51dfca7eba11a335dd688c0a847ba
Roger Silva <orogersilva@gmail.com>
2018-07-11 21:30:18 -0300

    Fix.

---
+++ feature-dashboard/src/main/kotlin/com/orogersilva/spotmusicalarm/featuredashboard/presentation/screen/dashboard/adapter/DashboardListAdapter.kt
@@ -0,0 +1,7 @@
+package com.orogersilva.spotmusicalarm.featuredashboard.presentation.screen.dashboard.adapter
+
+class DashboardListAdapter {
+}
+
+/*class ShoppingListSetAdapter @Inject constructor(private val shoppingListSetPresenter: ShoppingListSetContract.Presenter) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>(), ItemTouchHelperAdapter {*/
